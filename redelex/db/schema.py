73112a8f1bb6287759e0c5e7fd1f1dbf17226af4
Jakub Peleška <jakub.peleska@fel.cvut.cz>
2025-11-26 15:43:15 +0100

    Add abstract DBInterface

---
+++ redelex/db/schema.py
@@ -0,0 +1,75 @@
+from attrs import field, define
+from typing import List
+
+
+from .foreign_key import ForeignKey
+
+
+@define
+class DBSchema:
+    """
+    Represents the schema of a database.
+    """
+
+    table_schemas: dict[str, "TableSchema"] = field(converter=dict)
+    """
+    Schemas of individual tables, keyed by table name
+    """
+
+    @property
+    def table_names(self) -> List[str]:
+        """
+        The list of table names in the database
+        """
+        return list(self.table_schemas.keys())
+
+    def __str__(self):
+        out = ""
+        for i, tschema in enumerate(self.table_schemas.values()):
+            out += f"{i + 1}/{len(self.table_schemas)} {tschema}\n"
+        return out
+
+
+@define
+class TableSchema:
+    """
+    Represents the schema of one table.
+    """
+
+    name: str = field()
+    """
+    The name of the table
+    """
+
+    pk: List[str] = field(converter=list)
+    """
+    The primary key columns
+    """
+
+    fks: List[ForeignKey] = field(converter=list)
+    """
+    The foreign keys
+    """
+
+    type_dict: dict[str, str] = field(converter=dict)
+    """
+    A mapping from column names to their data types
+    """
+
+    @property
+    def columns(self) -> List[str]:
+        """
+        The list of column names
+        """
+        return list(self.type_dict.keys())
+
+    def __str__(self):
+        out = f"Table: {self.name}\n"
+        out += f"    Primary Key: {self.pk}\n"
+        out += "    Foreign Keys:\n"
+        for fk in self.fks:
+            out += f"    - {fk}\n"
+        out += "    Columns:\n"
+        for col, dtype in self.type_dict.items():
+            out += f"    - {col}: {dtype}\n"
+        return out
