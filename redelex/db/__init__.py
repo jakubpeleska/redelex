2814a2f1246eeea879bcdd2b1cf4594bf35259b5
Jakub Peleška <jakub.peleska@fel.cvut.cz>
2025-11-26 15:44:37 +0100

    Add exports

--- redelex/db/__init__.py
+++ redelex/db/__init__.py
@@ -1,4 +1,16 @@
 from .foreign_key import ForeignKey
 from .inspector import DBInspector
+from .interface import DBInterface
+from .relbench_db import RelbenchDBInterface
+from .remote_db import RemoteDBInterface
+from .schema import DBSchema, TableSchema
 
-__all__ = ["ForeignKey", "DBInspector"]
+__all__ = [
+    "ForeignKey",
+    "DBInspector",
+    "DBInterface",
+    "DBSchema",
+    "TableSchema",
+    "RelbenchDBInterface",
+    "RemoteDBInterface",
+]
