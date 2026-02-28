94244c2e68f47a809d245a5ee673e6f06b1d0705
Jakub Peleška <jakub.peleska@fel.cvut.cz>
2026-02-28 12:05:28 +0100

    Update nn.train utils

--- redelex/nn/train/utils.py
+++ redelex/nn/train/utils.py
@@ -11,7 +11,51 @@ from torchmetrics.classification import (
 )
 from torchmetrics.regression import MeanAbsoluteError, MeanSquaredError, R2Score
 
+from torch_geometric.data import HeteroData
+
 from relbench.base import TaskType
+
+
+def get_num_neighbors_dict(
+    data: HeteroData,
+    input_type: str,
+    base_neighbors: int,
+    num_layers: int,
+    depth_factor: int,
+) -> dict[str, list[int]]:
+    num_neighbors_dict: dict[tuple[str, str, str], list[int]] = {
+        e: [] for e in data.edge_types
+    }
+    layer_types = [input_type]
+    edge_types = [e for e in data.edge_types if e[2] in layer_types]
+    used_edge_types = []
+    for n in range(num_layers):
+        for e in data.edge_types:
+            num_neighbors_dict[e].append(0)
+        for edge_type in edge_types:
+            src_type, _, dst_type = edge_type
+            num_edges = len([e for e in edge_types if e[2] == dst_type])
+
+            num_neighbors = max(
+                1,
+                base_neighbors // (depth_factor**n) // num_edges,
+            )
+            num_neighbors_dict[edge_type][-1] += num_neighbors
+
+        used_edge_types.extend(edge_types)
+        layer_types = list(set([src_type for src_type, _, dst_type in edge_types]))
+        edge_types = [
+            e
+            for e in data.edge_types
+            if e[2] in layer_types
+            and all(
+                [
+                    not (e[0] == ue[0] and e[1] == ue[1] and e[2] == ue[2])
+                    for ue in used_edge_types
+                ]
+            )
+        ]
+    return num_neighbors_dict
 
 
 def get_metrics(
